fd2c9ab3d87f6eed83f0bfd1234bc31ac5f73154
Kécia Santos <keciakaroline@gmail.com>
2022-06-23 23:56:22 +0200

    Styled Footer component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,22 @@ import React, { useState } from "react";
 
 import ScrollToTop from "./ScrollToTop";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
 
 import "./App.css";
 
 function App() {
-  const [click, setClick] = useState(false);
-
   return (
     <>
-      <Navbar click={click} setClick={setClick} />
+      <Navbar />
       <ScrollToTop>
         <Routes>
-          {/* <Route exact path="/" element={<Homepage />}></Route> */}
+          <Route exact path="/" element={<Homepage />}></Route>
           {/* <Route exact path="/projects" element={<Homepage />}></Route>
           <Route exact path="/about" element={<Homepage />}></Route>
           <Route exact path="/contact" element={<Homepage />}></Route>
           <Route exact path="/" element={<Homepage />}></Route> */}
         </Routes>
-        <Footer />
       </ScrollToTop>
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,88 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./styles/Footer.css";
+import DeptLogo from "./styles/design/DEPT-LOGO-White.svg";
 
 export default function Footer() {
-  return <></>;
+  return (
+    <div className="Footer">
+      <div className="Footer-content">
+        <img src={DeptLogo} alt="DEPT logo white" className="footer-logo"></img>
+
+        <ul className="footer-menu">
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="nav-menu-link"
+              activeClassName="active"
+            >
+              WORK
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="nav-menu-link"
+              activeClassName="active"
+            >
+              SERVICES
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="nav-menu-link"
+              activeClassName="active"
+            >
+              STORIES
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="nav-menu-link"
+              activeClassName="active"
+            >
+              ABOUT
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="nav-menu-link"
+              activeClassName="active"
+            >
+              CAREERS
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              exact
+              to="/"
+              className="nav-menu-link"
+              activeClassName="active"
+            >
+              CONTACT
+            </NavLink>
+          </li>
+        </ul>
+
+        <hr />
+
+        <div>
+          <div className="footer-infos">
+            <div>Chamber of Commerce: 63464101 </div>
+            <div>VAT: NL 8552.47.502.B01</div>
+            <div>Terms and conditions</div>
+          </div>
+          <div className="copyright">2022 Dept Agency</div>
+        </div>
+      </div>
+    </div>
+  );
 }
