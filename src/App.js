cd9a0b862571747520e9d5ecae730de5e838227a
Kécia Santos <keciakaroline@gmail.com>
2022-06-23 21:30:45 +0200

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import ScrollToTop from "./ScrollToTop";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
+  const [click, setClick] = useState(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar click={click} setClick={setClick} />
       <ScrollToTop>
         <Routes>
-          <Route exact path="/" element={<Homepage />}></Route>
+          {/* <Route exact path="/" element={<Homepage />}></Route> */}
           {/* <Route exact path="/projects" element={<Homepage />}></Route>
           <Route exact path="/about" element={<Homepage />}></Route>
           <Route exact path="/contact" element={<Homepage />}></Route>
           <Route exact path="/" element={<Homepage />}></Route> */}
         </Routes>
+        <Footer />
       </ScrollToTop>
     </>
   );
