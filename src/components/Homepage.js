c097878a1832a346a8c8cf3d1396624d49023600
Kécia Santos <keciakaroline@gmail.com>
2022-06-25 01:47:29 +0200

    Added components to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,17 +3,26 @@ import Footer from "./Footer";
 import ClientList from "./ClientList";
 import ClientCard from "./ClientCard";
 import ClientQuote from "./ClientQuote";
-import Form from "./Form";
+import FormContact from "./FormContact";
+import ClientCard2 from "./ClientCard2";
+import ClientCard3 from "./ClientCard3";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+import "./styles/Homepage.css";
 
 export default function Homepage() {
   return (
-    <div>
-      Homepage
+    <div className="homepage">
+      {/* <Navbar /> */}
+      <Hero />
       <ClientCard />
+      <ClientCard2 />
       <ClientQuote />
+      <ClientCard3 />
       <ClientList />
-      <Form />
-      <Footer />
+      <FormContact />
+      {/* <Footer /> */}
     </div>
   );
 }
