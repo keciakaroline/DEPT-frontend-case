7ba80073a5d9da6ba5ce8a491bf6dc352ce5e7a5
Kécia Santos <keciakaroline@gmail.com>
2022-06-24 18:12:35 +0200

    Create form component and added some styles

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles/Form.css";
+
+export default function Form() {
+  return (
+    <div className="contact-out" id="contact">
+      <div className="contact">
+        <h1 className="contact-hero">QUESTION? WE ARE HERE TO HELP!</h1>
+        <form className="form"></form>
+      </div>
+    </div>
+  );
+}
