4e2abefc9c27c928c2c1356731377e048ae1ac2d
Kécia Santos <keciakaroline@gmail.com>
2022-06-24 11:48:54 +0200

    Added clientcard component to add carousel pictures

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "./Footer";
 import ClientList from "./ClientList";
+import ClientCard from "./ClientCard";
 
 export default function Homepage() {
   return (
     <div>
       Homepage
+      <ClientCard />
       <ClientList />
       <Footer />
     </div>
