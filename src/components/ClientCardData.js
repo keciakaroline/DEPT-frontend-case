9fde30ddfd6399bae5f035514464cab2e3bcb2e5
Kécia Santos <keciakaroline@gmail.com>
2022-06-25 01:47:09 +0200

    update data for client card

--- src/components/ClientCardData.js
+++ src/components/ClientCardData.js
@@ -2,17 +2,23 @@ import img4 from "./styles/design/clients-card/image4.png";
 import img5 from "./styles/design/clients-card/image5.png";
 import img6 from "./styles/design/clients-card/image6.png";
 import img7 from "./styles/design/clients-card/image7.png";
+import img8 from "./styles/design/clients-card/image8.png";
+import img9 from "./styles/design/clients-card/image9.png";
+import img10 from "./styles/design/clients-card/image10.png";
+import img11 from "./styles/design/clients-card/image11.png";
+import img12 from "./styles/design/clients-card/image12.png";
+import img13 from "./styles/design/clients-card/image13.png";
+import img14 from "./styles/design/clients-card/image14.png";
+import img15 from "./styles/design/clients-card/image15.png";
 
 export const imagesSet1 = [
   {
     title: "A Summer island in the Netherlands",
-
     brand: "BOL.COM",
     img: img4,
   },
   {
     title: "Not some average banking website",
-
     brand: "KEMPEN",
     img: img5,
   },
@@ -21,14 +27,64 @@ export const imagesSet1 = [
 export const imagesSet2 = [
   {
     title: "Beautiful design meets innovative technology",
-
     brand: "PHILIPS",
     img: img6,
   },
   {
     title: "A 100 years of Mondriaan & De Stijl",
-
     brand: "GEMEENTEMUSEUM",
     img: img7,
+  },
+];
+
+export const imagesSet3 = [
+  {
+    title: "Delivering clarity on a global scale",
+    brand: "BE LIGHTNING",
+    img: img8,
+  },
+  {
+    title: "Swipe to find your next holiday destination",
+    brand: "TUI",
+    img: img9,
+  },
+];
+
+export const imagesSet4 = [
+  {
+    title: "A campaign for the limited edition letter packs",
+    brand: "CHOCOMEL",
+    img: img10,
+  },
+  {
+    title: "Live like a champion with Jerome Boateng",
+    brand: "JBL",
+    img: img11,
+  },
+];
+
+export const imagesSet5 = [
+  {
+    title: "Innovative SEO and content strategy for Zalando",
+    brand: "ZALANDO",
+    img: img12,
+  },
+  {
+    title: "The search of the most influential book ever",
+    brand: "KONINKLIJKE BIBLIOTHEEK",
+    img: img13,
+  },
+];
+
+export const imagesSet6 = [
+  {
+    title: "Delivering complex commerce solutions",
+    brand: "LIBERTY GLOBAL ",
+    img: img14,
+  },
+  {
+    title: "Swipe to find your next holiday destination",
+    brand: "ARLA",
+    img: img15,
   },
 ];
