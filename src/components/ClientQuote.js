42a8a29f6d7b03486d76020a425a717c9897d21f
Kécia Santos <keciakaroline@gmail.com>
2022-06-24 18:12:12 +0200

    Added & finished clientQuote component

---
+++ src/components/ClientQuote.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/ClientQuote.css";
+
+export default function ClientQuote() {
+  return (
+    <div className="clientQuote-out">
+      <div className="clientQuote">
+        <p className="clientText">
+          “Dept helped us tell our story through a bold new identity and a
+          robust online experience. To the tone of 60% growth in online bookings
+          in just one month”
+        </p>
+        <h4 className="clientInfo">MATTIJS TEN DRINK - CEO, TRANSAVIA</h4>
+      </div>
+    </div>
+  );
+}
