95c4ac6678263438bb728abaf40458e16c87b473
Kécia Santos <keciakaroline@gmail.com>
2022-06-25 18:44:24 +0200

    fixed img in Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,16 @@
 import React from "react";
 
-import img0 from "./styles/design/clients-card/image0.png";
 import "./styles/Hero.css";
 
 export default function Hero() {
   return (
     <div className="hero-out">
-      <img src={img0} alt="" className="hero-img" />
+      <span className="hero-img"></span>
       <h1 className="hero-title">
         <span className="target-1">
           A selection of projects that <strong>pioneer tech</strong> and{" "}
           <strong>marketing</strong> to help brands stay ahead.
         </span>
-        {/* <span className="target-2"> */}
-        {/* </span> */}
       </h1>
 
       <p className="hero-brand">WORK</p>
