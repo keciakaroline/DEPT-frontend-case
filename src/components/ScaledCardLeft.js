ca6f8544b1f66552ce5248ca854b90238b33c4ca
Kécia Santos <keciakaroline@gmail.com>
2022-06-26 04:58:13 +0200

    up

--- src/components/ScaledCardLeft.js
+++ src/components/ScaledCardLeft.js
@@ -22,54 +22,48 @@ export default function ScaledCardLeft() {
       <div className="cardLeft-info-out">
         <div className="cardLeft-info">
           <div className="cardLeft-info-one">
-            <div className="info-one">
-              <p className="cardLeft-info-one-brand">MICROSOFT</p>
-              <h2 className="cardLeft-info-one-title">
-                Tapping into Ireland’s unique gaming culture and bringing a
-                fresh flavour to their Xbox social media channels
-              </h2>
-              <div className="read-more">
-                <img
-                  src={btnDot}
-                  className="btnDot"
-                  alt="dot symbol in front of 'read more'"
-                />
-                <a href="/">Read more</a>
-              </div>
+            <p className="cardLeft-info-one-brand">MICROSOFT</p>
+            <h2 className="cardLeft-info-one-title">
+              Tapping into Ireland’s unique gaming culture and bringing a fresh
+              flavour to their Xbox social media channels
+            </h2>
+            <div className="read-more">
+              <img
+                src={btnDot}
+                className="btnDot"
+                alt="dot symbol in front of 'read more'"
+              />
+              <a href="/">Read more</a>
             </div>
           </div>
           <div className="cardLeft-info-two">
-            <div className="info-two">
-              <p className="cardLeft-info-two-brand">O’NEILL</p>
-              <h2 className="cardLeft-info-two-title">
-                Integrating existing content into O’Neills’s new e-commerce
-                platform
-              </h2>
-              <div className="read-more">
-                <img
-                  src={btnDot}
-                  className="btnDot"
-                  alt="dot symbol in front of 'read more'"
-                />
-                <a href="/">Read more</a>
-              </div>
+            <p className="cardLeft-info-two-brand">O’NEILL</p>
+            <h2 className="cardLeft-info-two-title">
+              Integrating existing content into O’Neills’s new e-commerce
+              platform
+            </h2>
+            <div className="read-more">
+              <img
+                src={btnDot}
+                className="btnDot"
+                alt="dot symbol in front of 'read more'"
+              />
+              <a href="/">Read more</a>
             </div>
           </div>
           <div className="cardLeft-info-three">
-            <div className="info-three">
-              <p className="cardLeft-info-three-brand">MICROSOFT</p>
-              <h2 className="cardLeft-info-three-title">
-                Tapping into Ireland’s unique gaming culture and bringing a
-                fresh flavour to their Xbox social media channels
-              </h2>
-              <div className="read-more">
-                <img
-                  src={btnDot}
-                  className="btnDot"
-                  alt="dot symbol in front of 'read more'"
-                />
-                <a href="/">Read more</a>
-              </div>
+            <p className="cardLeft-info-three-brand">MICROSOFT</p>
+            <h2 className="cardLeft-info-three-title">
+              Tapping into Ireland’s unique gaming culture and bringing a fresh
+              flavour to their Xbox social media channels
+            </h2>
+            <div className="read-more">
+              <img
+                src={btnDot}
+                className="btnDot"
+                alt="dot symbol in front of 'read more'"
+              />
+              <a href="/">Read more</a>
             </div>
           </div>
         </div>
