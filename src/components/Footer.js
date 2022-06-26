2c3480e8e63eee006d5afa9724f9bc03e4306c22
Kécia Santos <keciakaroline@gmail.com>
2022-06-26 17:58:37 +0200

    Fixed mobile size & added socials links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 import "./styles/Footer.css";
 import DeptLogo from "./styles/design/DEPT-LOGO-White.svg";
 import ArrowUp from "./styles/design/arrow-up.svg";
+import facebook from "./styles/design/facebook.png";
+import twitter from "./styles/design/twitter.png";
+import instagram from "./styles/design/instagram.png";
 
 export default function Footer() {
   const goToTop = () => {
@@ -89,11 +92,42 @@ export default function Footer() {
             <div>VAT: NL 8552.47.502.B01</div>
             <div>Terms and conditions</div>
           </div>
-          <div className="copyright">2022 Dept Agency</div>
+          <div className="copyright">© 2022 Dept Agency</div>
         </div>
         <div className="arrow-up">
           <img src={ArrowUp} alt="" onClick={goToTop}></img>
           <p>TOP</p>
+        </div>
+
+        <div className="for-mobile-socials">
+          <div className="facebook">
+            <a
+              href="https://www.facebook.com/DeptAgency/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <img src={facebook} alt="facebook symbol" />{" "}
+            </a>
+          </div>
+          <div className="twitter">
+            <a
+              href="https://twitter.com/deptagency?lang=en"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={twitter} alt="twitter symbol" />
+            </a>
+          </div>
+          <div className="instagram">
+            <a
+              href="https://www.instagram.com/deptagency/?hl=en"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instagram} alt="instagram symbol" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
